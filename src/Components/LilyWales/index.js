ab9ade4c63d3a53a8f926d720c324e1908a5e7c3
Lily Wales <lilywales@lilys-mbp.home>
2018-03-11 15:07:03 +0000

    images and titles linked in state

--- src/Components/LilyWales/index.js
+++ src/Components/LilyWales/index.js
@@ -1,25 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 const styles = {
-  lilywales: {
-    position: 'absolute',
-    top: '2%',
-    right: '0.85%',
-    fontSize: '5.5rem',
-    // fontWeight: '800',
-    color: 'rgb(255, 5, 125)'
-
-  }
-}
+	lilywales: {
+		position: 'absolute',
+		top: '2%',
+		right: '0.85%',
+		fontSize: '5.5rem',
+		// fontWeight: '800',
+		color: 'rgb(255, 5, 125)'
+	}
+};
 
 class LilyWales extends Component {
-  render() {
-    return (
-      <div>
-        <h1 style={styles.lilywales} alt="LILY WALES"> LILY WALES</h1>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<h1 style={styles.lilywales} alt="LILY WALES">
+					{' '}
+					LILY WALES
+				</h1>
+			</div>
+		);
+	}
 }
 
 export default LilyWales;
