ae73144c2fc598f0848d3e706387005b1c36e88a
Lily Wales <lilywales@Lilys-MacBook-Pro.local>
2018-03-20 11:55:27 +0000

    desktop WM

---
+++ src/Components/HomeButton/colours.js
@@ -0,0 +1,11 @@
+export const green = '#3bff1b';
+export const red = '#ff0000';
+export const navy = '#0021ff';
+
+export const colours = {
+	green,
+	red,
+	navy
+};
+
+export default colours;
---
+++ src/Components/WMtext/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './WMtext.scss';
+
+const styles = {
+	wmWrapper: {
+		position: 'absolute',
+		right: '5.5%',
+		top: '35%',
+		width: '240px'
+	},
+	wmDescription: {
+		textAlign: 'right',
+		color: 'rgb(245, 27, 236)',
+		fontSize: '1.5rem'
+	},
+	wmTitle: {
+		position: 'absolute',
+		right: '5.5%',
+		color: 'rgb(245, 27, 236)',
+		top: '107px',
+		fontSize: '3.3rem'
+	}
+};
+
+class WMtext extends Component {
+	render() {
+		return (
+			<div>
+				<h1 className="wmTitle" style={styles.wmTitle}>
+					WAR MACHINE
+				</h1>
+				<div className="wmWrapper" style={styles.wmWrapper}>
+					<h3 className="wmDescription" style={styles.wmDescription}>
+						In this series the<br />
+						notion of war is<br />explored through<br />the juxtaposition<br />
+						of predatory<br />
+						instinct and the<br />need for survival.<br />Selected artworks
+						<br />
+						exhibited at The<br />
+						Photographers<br />Wall & Argentea<br />Gallery.
+					</h3>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default WMtext;
