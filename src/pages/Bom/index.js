1627f362902144b8c0ff7f80fc22cb7a1c89d933
Lily Wales <lilywales@lilys-mbp.home>
2018-05-21 18:52:06 +0100

    rec ret started

--- src/pages/Bom/index.js
+++ src/pages/Bom/index.js
@@ -12,8 +12,8 @@ class Bom extends Component {
 			<div>
 				<ProjectTitle title="Recreational Retina" color="rgb(12,206,107)" />
 				<LilyWales color="rgb(255,114,0)" />
-				<BackToTop color="rgb(255,114,0)" />
-				<HomeButton color="rgb(255,114,0)" />
+				<BackToTop color="rgb(255,114,0)" top="264%" right="5.5%" />
+				<HomeButton color="rgb(255,114,0)" top="269%" right="5.5%" />
 				<BomContent />
 			</div>
 		);
